3f420af47a050561f9b86021e7c60de530f691b4
Sharren <sharrenepf@gmail.com>
2025-03-23 12:35:48 +0700

    percobaan 1

---
+++ Sorting24.java
@@ -0,0 +1,32 @@
+public class Sorting24 {
+    int[] data;
+    int jumData;
+
+    Sorting24(int Data[], int jmlDat) {
+        jumData = jmlDat;
+        data = new int[jmlDat];
+        for (int i = 0; i < jumData; i++) {
+            data[i] = Data[i];
+        }
+    }
+
+    void bubbleSort() {
+        int temp;
+        for (int i = 0; i < jumData - 1; i++) {
+            for (int j = 0; j < jumData - 1 - i; j++) {
+                if (data[j] > data[j + 1]) {
+                    temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    void tampil() {
+        for (int i = 0; i < jumData; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+    }
+}    
