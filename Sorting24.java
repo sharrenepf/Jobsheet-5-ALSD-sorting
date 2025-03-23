addcccb8165dd6cf8239b145d307295f66c55872
Sharren <sharrenepf@gmail.com>
2025-03-23 13:40:38 +0700

    percobaan 2

--- Sorting24.java
+++ Sorting24.java
@@ -13,7 +13,7 @@ public class Sorting24 {
     void bubbleSort() {
         int temp;
         for (int i = 0; i < jumData - 1; i++) {
-            for (int j = 0; j < jumData - 1 - i; j++) {
+            for (int j = 0; j < jumData - 1; j++) {
                 if (data[j] > data[j + 1]) {
                     temp = data[j];
                     data[j] = data[j + 1];
@@ -23,10 +23,24 @@ public class Sorting24 {
         }
     }
 
+    void SelectionSort() {
+        for (int i = 0; i < jumData - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < jumData; j++) {
+                if (data[j] < data[min]) {
+                    min = j;
+                }
+            }
+            int temp = data[i];
+            data[i] = data[min];
+            data[min] = temp;
+        }
+    }
+
     void tampil() {
         for (int i = 0; i < jumData; i++) {
             System.out.print(data[i] + " ");
         }
         System.out.println();
     }
-}    
+}
--- SortingMain24.java
+++ SortingMain24.java
@@ -4,12 +4,24 @@ public class SortingMain24 {
 
         Sorting24 dataurut1 = new Sorting24(a, a.length);
         
-        System.out.println("Data awal:");
+        System.out.println("Data awal 1:");
         dataurut1.tampil();
 
         dataurut1.bubbleSort();
         
         System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC):");
         dataurut1.tampil();
+        
+        int b[] = {30, 20, 2, 8, 14};
+        
+        Sorting24 dataurut2 = new Sorting24(b, b.length);
+        
+        System.out.println("Data awal 2:");
+        dataurut2.tampil();
+        
+        dataurut2.SelectionSort();
+        
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC):");
+        dataurut2.tampil();
     }
 }
